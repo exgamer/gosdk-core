1eb2a3168062713d70ed1e56580a1ea71a18acda
olzhaskulzhambekov <okulzhambekov@jmart.kz>
2025-12-26 09:28:03 +0500

    init

--- pkg/exception/app_exception.go
+++ pkg/exception/app_exception.go
@@ -2,10 +2,11 @@ package exception
 
 // AppException Модель данных для описания ошибки
 type AppException struct {
-	Error   error
-	Context map[string]any
+	Error         error
+	Context       map[string]any
+	TrackInSentry bool
 }
 
-func NewAppException(err error, context map[string]any) *AppException {
-	return &AppException{err, context}
+func NewAppException(err error, context map[string]any, trackInSentry bool) *AppException {
+	return &AppException{err, context, trackInSentry}
 }
