357360880131193d9bdd98325f2a027db92d25ee
olzhaskulzhambekov <okulzhambekov@jmart.kz>
2026-01-12 09:25:12 +0500

    init

--- pkg/exception/app_exception.go
+++ pkg/exception/app_exception.go
@@ -1,12 +1,24 @@
 package exception
 
+func NewAppException(err error, context map[string]any, trackInSentry bool) *AppException {
+	return &AppException{err, context, trackInSentry}
+}
+
 // AppException Модель данных для описания ошибки
 type AppException struct {
-	Error         error
+	Err           error
 	Context       map[string]any
 	TrackInSentry bool
 }
 
-func NewAppException(err error, context map[string]any, trackInSentry bool) *AppException {
-	return &AppException{err, context, trackInSentry}
+func (e *AppException) Error() string {
+	if e.Err == nil {
+		return "unknown application exception"
+	}
+
+	return e.Err.Error()
+}
+
+func (e *AppException) Unwrap() error {
+	return e.Err
 }
